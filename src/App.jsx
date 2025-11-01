f214faa017d506378dd63efbbfc880ee95ea4ba3
khaledadawi <adawikhaled390@gmail.com>
2025-11-01 23:49:26 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from './components/navbar/Navbar'
+import Hero from './components/hero/Hero'
+import Portfolio from './components/portfolio/Portfolio'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+
+
+
+function App(){
+
+  return <>
+    <Navbar />
+    <Hero />
+    <Portfolio />
+    <About />
+    <Contact />
+    <Footer />
+  </>
+}
+
+export default App;
